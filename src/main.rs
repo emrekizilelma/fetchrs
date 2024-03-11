11417bfbb1daabc661b91895ce5ee3fac63f3754
emrekizilelma <emrekizilelma00@gmail.com>
2024-03-11 10:30:27 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use sysinfo::System;
 
 fn main() {
-    println!("CPU Arch:          {:?}", System::cpu_arch());
+
+    let total_mem = System::new_all();
+
     println!("System name:       {:?}", System::name());
     println!("System OS version: {:?}", System::os_version());
-    println!("System host name:  {:?}", System::host_name());
+    println!("CPU Arch:          {:?}", System::cpu_arch());
+    println!("Kernel version:    {:?}", System::kernel_version());
+    println!("Total Memory:      {}", total_mem.total_memory());
 }
