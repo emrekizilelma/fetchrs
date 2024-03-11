305a678523319c85879051b2e1c00c0d8895a87d
emrekizilelma <emrekizilelma00@gmail.com>
2024-03-11 11:04:37 +0300

    unwrapped

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
 
     let total_mem = System::new_all();
 
-    println!("System name:       {:?}", System::name());
-    println!("System OS version: {:?}", System::os_version());
-    println!("CPU Arch:          {:?}", System::cpu_arch());
-    println!("Kernel version:    {:?}", System::kernel_version());
+    println!("System name:       {:?}", System::name().unwrap());
+    println!("System OS version: {:?}", System::os_version().unwrap());
+    println!("CPU Arch:          {:?}", System::cpu_arch().unwrap());
+    println!("Kernel version:    {:?}", System::kernel_version().unwrap());
     println!("Total Memory:      {}", total_mem.total_memory());
 }
