be9df1b434c4bc1043fe2c78cfdb4afe1d78e5fc
emrekizilelma <emrekizilelma00@gmail.com>
2025-04-02 19:32:17 +0300

    add: host

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use sysinfo::System;
 fn main() {
 
     let total_mem = System::new_all();
-
+    
     println!("System name:       {}", System::name().unwrap());
     println!("System OS version: {}", System::os_version().unwrap());
     println!("CPU Arch:          {}", System::cpu_arch().unwrap());
     println!("Kernel version:    {}", System::kernel_version().unwrap());
+    println!("Hostname           {:?}", System::host_name().unwrap());
     println!("Total Memory:      {}", total_mem.total_memory());
 }
